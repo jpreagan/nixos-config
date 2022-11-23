a199252506487afc2434619b72b7181e6649a87c
James Reagan <james@jpreagan.com>
2022-11-22 18:01:14 -1000

    Enable chromium extensions

--- system/chromium.nix
+++ system/chromium.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
 {
-  programs.chromium.enable = true;
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+      "gcbommkclmclpchllfjekcdonpmejbdp" # HTTPS Everywhere
+      "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
+      "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+    ];
+  };
 
   environment.systemPackages = with pkgs; [ ungoogled-chromium ];
 }
