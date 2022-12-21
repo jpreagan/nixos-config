5db097646fa87404e934a7d49eafe374635ee912
James Reagan <james@jpreagan.com>
2022-12-21 09:09:28 -1000

    Add Jest and GitHub Copilot extensions

--- home/vscode.nix
+++ home/vscode.nix
@@ -7,16 +7,25 @@
       [
         dbaeumer.vscode-eslint
         esbenp.prettier-vscode
+        github.copilot
         github.github-vscode-theme
         golang.go
         ms-azuretools.vscode-docker
         prisma.prisma
         vscodevim.vim
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-        name = "tokyo-night";
-        publisher = "enkia";
-        version = "0.9.4";
-        sha256 = "0v1hkv8smwp64c0danrln39h0lgqa2dwiiy45rx29s9qmq3j9am4";
-      }];
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "tokyo-night";
+          publisher = "enkia";
+          version = "0.9.4";
+          sha256 = "0v1hkv8smwp64c0danrln39h0lgqa2dwiiy45rx29s9qmq3j9am4";
+        }
+        {
+          name = "vscode-jest";
+          publisher = "Orta";
+          version = "5.1.0";
+          sha256 = "08b3jrwm3rrwkkq8ybiak6r9d7hp4whjnpdji72jy0344xid1nqa";
+        }
+      ];
   };
 }
