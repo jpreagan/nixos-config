c283727a9661091d98ecb06e7bf6e232c5f7d212
James Reagan <james@jpreagan.com>
2023-02-02 10:57:17 -1000

    Add virtualbox

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,4 +1,13 @@
+{ pkgs, ... }:
+
 {
   virtualisation.docker.enable = true;
   users.users.james.extraGroups = [ "docker" ];
+  environment.systemPackages = with pkgs; [ docker-compose ];
+
+  virtualisation.virtualbox.host = { 
+    enable = true;
+    enableExtensionPack = true;
+  };
+  users.extraGroups.vboxusers.members = [ "james" ];
 }
