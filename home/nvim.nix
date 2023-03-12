5204f1b94233ffcdbcdcd58a4b725f34ee36d3f4
James Reagan <james@jpreagan.com>
2023-03-12 13:41:38 -1000

    Use neovim

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,27 +1,10 @@
 { pkgs, ... }:
 
 {
-  home.shellAliases = {
-    vi = "lvim";
-    vim = "lvim";
-  };
-
   programs.neovim = {
     enable = true;
-    withNodeJs = true;
-    extraPackages = with pkgs; [
-      nodePackages.neovim
-      tree-sitter
-      python39Packages.pynvim
-
-      # Nix
-      rnix-lsp
-
-      # Lua
-      sumneko-lua-language-server
-
-      # TypeScript
-      nodePackages.typescript-language-server
-    ];
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
   };
 }
