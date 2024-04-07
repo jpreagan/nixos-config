a265db12636071573bc631e851b6b83b9c259ba1
James Reagan <james@jpreagan.com>
2024-04-06 16:19:56 -1000

    Remove packages

--- home/packages.nix
+++ home/packages.nix
@@ -4,41 +4,19 @@
   programs.go.enable = true;
 
   home.packages = with pkgs; [
-    awscli2
-    azure-cli
     colordiff
     exiftool
     fd
-    flyctl
     gh
-    github-copilot-cli
-    google-cloud-sdk
     htop
     imagemagick
-    mkcert
     neofetch
-    nss
     pandoc
-    pscale
     pwgen
-    sqlc
-    sqlfluff
     tldr
     tree
-    nodePackages.vercel
     ripgrep
     wget
-    zola
-
-    # C/C++
-    gcc
-    gnumake
-
-    # Java
-    jdk20
-
-    # Nix
-    nixfmt
 
     # Lua
     stylua
@@ -46,28 +24,6 @@
     # Go
     gopls
     go-tools
-
-    # JavaScript
-    bun
-    deno
-    nodejs
-    nodePackages.yarn
-    nodePackages.pnpm
-
-    # PHP
-    php82
-    php82Packages.composer
-
-    # Python
-    (python311.withPackages (ps: with ps; [
-      black
-      #jupyterlab
-      pip
-      pynvim
-      requests
-      virtualenv
-    ]))
-    pipenv
 
     # Rust
     rustup
