a8c33691f689e69f0672ece75e3dfb2d0c279a0e
James Reagan <james@jpreagan.com>
2024-04-06 16:18:49 -1000

    Use home directory `/Users` for darwin

--- home/default.nix
+++ home/default.nix
@@ -5,10 +5,9 @@
     [ ./zsh.nix ./kitty.nix ./git.nix ./nvim.nix ./packages.nix ];
 
   home.username = "james";
-  home.homeDirectory = "/Users/james";
+  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/james" else "/home/james";
 
   home.stateVersion = "22.11";
 
   programs.home-manager.enable = true;
 }
-
