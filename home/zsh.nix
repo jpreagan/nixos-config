1603085ef639802edbceaa52f0020f20fb25498e
James Reagan <james@jpreagan.com>
2024-04-19 18:26:40 -1000

    Update PATH

--- home/zsh.nix
+++ home/zsh.nix
@@ -27,12 +27,15 @@
     initExtra = ''
       PURE_PROMPT_SYMBOL='▶'
       PURE_PROMPT_VICMD_SYMBOL='◀'
+      source ~/.local/share/google-cloud-sdk/completion.zsh.inc
     '';
     profileExtra = ''
       path=($HOME/bin
             $HOME/.local/bin
+            $HOME/.local/share/google-cloud-sdk/bin
             $HOME/.cargo/bin
             $HOME/Library/Python/3.12/bin
+            /Library/Frameworks/Python.framework/Versions/3.12/bin
             /opt/local/bin
             $path)
     '';
