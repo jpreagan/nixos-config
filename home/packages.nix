080c11be60828589693d251816e8ec91a47a9b3a
James Reagan <james@jpreagan.com>
2024-05-19 16:19:19 -1000

    Update packages

--- home/packages.nix
+++ home/packages.nix
@@ -1,29 +1,48 @@
 { pkgs, ... }:
 
 {
-  programs.go.enable = true;
+  programs.go = {
+    enable = true;
+    goBin = ".local/bin";
+  };
 
   home.packages = with pkgs; [
     colordiff
     exiftool
     fd
+    flyctl
     gh
     htop
     imagemagick
     neofetch
     pandoc
+    pure-prompt
     pwgen
     tldr
     tree
     ripgrep
     wget
 
-    # Lua
-    stylua
-
     # Go
     gopls
     go-tools
+
+    # Java
+    jdk
+    maven
+
+    # Lua
+    stylua
+
+    # Nix
+    nixd
+    nixpkgs-fmt
+
+    # Python
+    python312
+    python312Packages.pip
+    poetry
+    python312Packages.huggingface-hub
 
     # Rust
     rustup
